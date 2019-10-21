bdadb6d55059b456db32eea222ceefa959370140
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-21 12:36:45 +0800

    - when the button click the number will be 1 instead of 0

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick= {() => {
-          alert('now you know how to handle onClick event')
-        }}>
-          Hello world!
-        </button>
-        <p>number: 0</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {number: 0};
+  }
+  render() {
+    return (
+      <div className="App">
+          <button onClick={() => 
+          { this.setState({number: 1})}}>
+            +
+          </button >
+          <p>number: {this.state.number}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
