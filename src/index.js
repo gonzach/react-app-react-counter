2e901b3c7194f19e5f8b3f6262422011fe774da3
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-21 11:51:20 +0800

    - Hello world in index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const hello = <button>Hello World!</button>
+const hello = <div><button>Hello World!</button></div>
 
-ReactDOM.render(<hello />, document.getElementById('root'));
+ReactDOM.render(hello, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
