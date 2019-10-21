219f8df67ed248170436f102909312db7ae6818b
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-21 11:53:28 +0800

    - revert change in index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const hello = <div><button>Hello World!</button></div>
-
-ReactDOM.render(hello, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
